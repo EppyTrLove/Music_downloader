61f3d7fe1732a28fa29d7feff8f84f8bd8a66d8c
Nikita Epifantsev <eppy_92@mail.ru>
2023-06-15 15:37:04 +0300

    second commit

--- Music_downloader/PostModel.cs
+++ Music_downloader/PostModel.cs
@@ -1,18 +1,44 @@
-﻿
-namespace Music_downloader
+﻿namespace Music_downloader
 {
-    public class PostModel
+    public class PostModel : IEqualityComparer<PostModel>
     {
-        public PostModel(string link, string name, int likes, int reposts)
+        public PostModel(long id, string name, int likes, int reposts, DateTime date)
         {
-            Link = link;
+            Id = id;
             Name = name;
             Likes = likes;
             Reposts = reposts;
+            Date = date;
         }
-        public string Link { get; set; }
+        public long Id { get; set; }
         public string Name { get; set; }
         public int Likes { get; set; }
         public int Reposts { get; set; }
+        public DateTime Date { get; set; }
+        public AttachmentModel[] Attachments { get; set; }
+        public PersonModel Person { get; set; }
+
+        public bool Equals(PostModel? x, PostModel? y)
+        {
+            if (ReferenceEquals(x, y)) return true;
+            if (ReferenceEquals(x, null)) return false;
+            if (ReferenceEquals(y, null)) return false;
+            if (x.GetType() != y.GetType()) return false;
+            return x.Id == y.Id;
+        }
+
+        public int GetHashCode(PostModel obj)
+        {
+            return obj.Id.GetHashCode();
+        }
+
+        public class AttachmentModel
+        {
+            public string Link { get; set; }
+        }
+        public class PersonModel
+        {
+            public long id { get; set; }
+        }
     }
 }
--- Music_downloader/Program.cs
+++ Music_downloader/Program.cs
@@ -1,11 +1,16 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Music_downloader;
+using System.Diagnostics;
+using System.Net.Mail;
 using VkNet;
 using VkNet.Enums.Filters;
 using VkNet.Model;
+using VkNet.Model.Attachments;
 using VkNet.Model.RequestParams;
+using static Music_downloader.PostModel;
 
-class Program {
+class Program
+{
     public static void Main(string[] args)
     {
         var config = new ConfigurationBuilder()
@@ -21,27 +26,89 @@ class Program {
             Settings = Settings.All,
             ApplicationId = 51661636
         });
-        Console.WriteLine(client.Token);
+        Console.WriteLine("Helo my friend! Your VK Token =" + client.Token + "\nPlease enter name of the group you want " +
+            "to scan:");
+        var inputValue = Console.ReadLine();
 
-        var groups = client.Groups.GetById(new [] {"grimeofficial"}, null, GroupsFields.All).FirstOrDefault();
-        var get = client.Wall.Get(new WallGetParams
+        var groups = client.Groups.GetById(new[] { inputValue }, null, GroupsFields.All).FirstOrDefault();
+
+        ulong offset = 0;
+        var get = new List<WallGetObject>();
+        var wallPosts = new WallGetObject(); // переименовать если оставляем
+        do
+        {
+            wallPosts = client.Wall.Get(new WallGetParams
+            {
+                OwnerId = -groups.Id,
+                Offset = offset,
+                Count = 100,
+            });
+            offset += 100;
+            get.Add(wallPosts);
+        }
+        while (wallPosts.TotalCount == 100);
+
+        var getPersonId = client.Likes.GetList(new LikesGetListParams
         {
             OwnerId = -groups.Id,
-            Offset = 18338,
+            Offset = 18348,
+            Count = 100,
         });
-        var postModelDao = new PostModelDao($@"{Environment.CurrentDirectory}\Database.txt");
-        var data = postModelDao.GetAll().ToList();
+
+        var postModelDao = new PostModelDao($@"{Environment.CurrentDirectory}\MainDatabase.txt");
+        var postModelDaoPerson = new PostModelDao($@"{Environment.CurrentDirectory}\PersonDatabase.txt");
+        var mainDdata = postModelDao.GetAll().ToList();
+        var PersonData = postModelDaoPerson.GetAll().ToList();
         var localData = new List<PostModel>();
 
-        foreach (var post in get.WallPosts)
-            if (post.Attachment.Type.FullName == "VkNet.Model.Attachments.Video" ||
-                !data.Any(x => x.GetHashCode() == post.GetHashCode()))
-                localData.Add(new PostModel(post., post.Likes.Count, post.Reposts.Count))
-            else break;
+        foreach (var wallPost in get)
+            foreach (var i in wallPost.WallPosts) //Цикл не работает как нужно. Чем заменить вложенность?
+            {
+                if (mainDdata.Any(x => Equals(x.Id, wallPost.Id)))
+                    continue;
+
+                var post = new PostModel(wallPost.Id!.Value, wallPost.Text, wallPost.Likes.Count,
+                    wallPost.Reposts.Count, wallPost.Date.Value);
+                var attachments = new List<AttachmentModel>();
+                foreach (var attachment in wallPost.Attachments)
+                {
+                    if (attachment.Type.FullName == "VkNet.Model.Attachments.Document")
+                    {
+                        attachments.Add(new AttachmentModel { Link = (attachment.Instance as Document).Uri });
+                    }
+                }
+                if (attachments.Any())
+                {
+                    post.Attachments = attachments.ToArray();
+                    localData.Add(post);
+                }
+            }
+        postModelDao.Add(localData.ToArray());
+        var result = new HttpClient()
+        .GetByteArrayAsync(
+            "https://vk.com/doc65735415_298672804?hash=CzZfy0SeDEuY255lCngJO2sxZqWwlvThnngr15gXZcT&dl=f2P7ZxrU3aw23DMU41RXgowhDOECcVRmrBI9UnbygJs&api=1&no_preview=1")
+            .Result;
+        Console.WriteLine("Enter the path of directory to save the attachment:");
+        inputValue = Console.ReadLine();
+        File.WriteAllBytes(inputValue! + @"\test.rar", result);
+        Environment.CurrentDirectory = inputValue!;
+        var enrchivator = Process.Start(@"C:\Program Files\7-Zip\7z.exe", new[] { "x", "test.rar", "-o\".\"" });
+        while (!enrchivator.HasExited)
+        {
+            Thread.Sleep(100);
+        }
+
+        // Console.WriteLine(enrchivator.StandardOutput.ReadToEnd());
+        if (enrchivator.ExitCode != 0)
+        {
+            Console.WriteLine("Error!!! ExitCode=" + enrchivator.ExitCode);
+        }
+
     }
+
 }
 
-  
+
 
 
 
